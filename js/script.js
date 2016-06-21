19a2c83d792d26a65a319997c90e75f8577d2e43
Misacorp <misa.jokisalo@gmail.com>
2016-06-21 12:55:48 +0300

    Navbar

--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
  $(document).ready(function(){
       $('.parallax').parallax();
+      $(".button-collapse").sideNav();
     });
-
- $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
-
-/*
-$(document).ready(function() {
- if($(window).width() < 600) 
-  // This will remove the tooltip functionality for the buttons on this page
-  $('.tooltipped').tooltip('remove');
- }); */
