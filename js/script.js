9ef03f30977713e0a407ac43bd9cbe681e0518e2
Misacorp <misa.jokisalo@gmail.com>
2016-06-21 16:27:15 +0300

    Footer and app icons

--- js/script.js
+++ js/script.js
@@ -2,3 +2,18 @@
       $('.parallax').parallax();
       $(".button-collapse").sideNav();
     });
+
+ $(document).one("scroll", function() {
+      $('#posti').text(rakennaPosti("kö","säh","i.com","post"));
+      $('#puhelin').text(rakennaPuh("67","3 45","50 12","+358 "));
+    });
+
+var rakennaPosti = function(username2, username1, domain2, domain1) {
+  var posti = username1+username2+"@"+domain1+domain2;
+  return posti;
+}
+
+var rakennaPuh = function(osa4, osa3, osa2, osa1) {
+  var puh = osa1 + osa2 + osa3 + osa4;
+  return puh;
+}
