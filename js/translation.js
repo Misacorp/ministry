ee201d1b166d3b54ce8d7c7c52c70b64245177b6
Misacorp <misa.jokisalo@gmail.com>
2016-07-13 09:46:13 +0300

    More content

--- js/translation.js
+++ js/translation.js
@@ -1,14 +1,15 @@
-var etusivu_q_01_eng = "Non profit organization- love, lead and lift people to live life fully the way God planned it.";
+var etusivu_q_01_eng = "A nondenominational organization focused on evangelism, lifting up and supporting the local church.";
 var etusivu_q_02_eng = "Our mission is to reach the nations- meet inviduals.";
 
-var etusivu_p_01_eng = "English paragraph 01 here.";
-var etusivu_p_02_eng = "English paragraph 02 here.";
+var etusivu_p_01_eng = "The purpose of the Keys of the Kingdom organization is to awaken this generation to find its true identity in Christ. It encourages people to use their own gifts and dreams to concretely love and help others. The organization not only serves in Finland, but also overseas.";
+var etusivu_p_02_eng = "We visit different churches, schools, organizations, and events, and we also organize events in cooperation with others. We seek to build unity between individuals and churches, guiding young people in finding a deeper meaning to life along with a connection to church.";
 
 
 var avustustyo_q_01_eng = "And now abide faith. hope. love, these three; but the greatest of these is LOVE ( 1. Kor.13:13)";
-var avustustyo_q_02_eng = "Therefore be imitators of God as dear children. And walk in love, as Christ also has loved us and given himself for us, an offering and a sacrifice to God for a sweet- smelling aroma Ef.5.1-2";
+var avustustyo_q_02_eng = "Therefore be imitators of God as dear children. And walk in love, as Christ also has loved us and given himself for us... Ef.5.1-2";
 
-var avustustyo_p_01_eng = "English avustustyö text here.";
+var avustustyo_p_01_eng = "Keys of the Kingdom also provides financial support, concretely helping in situations of need. We support orphans, the poor and those in need. If need be, we will build and support orphanages, schools and more.";
+var avustustyo_p_02_eng = "Keys of the Kingdom also offers food and clothing aid to young people and families.";
 
 
 
